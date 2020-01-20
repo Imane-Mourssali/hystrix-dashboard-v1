557a30cf592f2ef393c873acd4ea66838b47ffa1
Imane-Mourssali <57915785+Imane-Mourssali@users.noreply.github.com>
2020-01-20 20:08:42 +0000

    Initial commit

---
+++ src/main/java/org/sid/hystrixdashboardv1/HystrixDashboardV1Application.java
@@ -0,0 +1,15 @@
+package org.sid.hystrixdashboardv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/sid/hystrixdashboardv1/HystrixDashboardV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.hystrixdashboardv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
